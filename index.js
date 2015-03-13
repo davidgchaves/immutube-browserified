3b300a4ff8b0d0d052cf91c66616f56d02fb4c74
David G Chaves <davidgchaves@gmail.com>
2015-03-13 14:33:19 +0100

    Browserify and Ramda play nice with each other

---
+++ index.js
@@ -0,0 +1,5 @@
+var app  = require('./lib/app'),
+    data = require('./lib/people');
+
+app.findSupermanInLog(data.mrKentIsInThaHouse);
+
--- test/initial_spec.js
+++ test/initial_spec.js
@@ -3,3 +3,15 @@
 var chai   = require('chai'),
     expect = chai.expect;
 
+var app  = require('../lib/app'),
+    data = require('../lib/people');
+
+describe('require(whatever)', function () {
+
+  it('works with Superman example', function () {
+    expect(app.findSupermanIn(data.mrKentIsInThaHouse)).to.be.equal('Clark Kent');
+    expect(app.findSupermanIn(data.mrKentHasLeftTheBuilding)).to.be.undefined;
+  });
+
+});
+
