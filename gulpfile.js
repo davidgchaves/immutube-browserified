1d39b6580333ad3b63b3cb62a8f2ac53319658f1
David G Chaves <davidgchaves@gmail.com>
2015-03-14 13:40:00 +0100

    Add gulp-browserify, 'browserify-client', and 'browserify-test' tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@
 var gulp   = require('gulp');
 var rename = require('gulp-rename');
 
+// Build Dependencies
+var browserify = require('gulp-browserify');
+
 // Dev Dependencies
 var jshint = require('gulp-jshint');
 
@@ -18,3 +21,19 @@ gulp.task('lint-test', function() {
     .pipe(jshint.reporter('default'));
 });
 
+// Browserify Tasks
+gulp.task('browserify-client', ['lint-client'], function() {
+  return gulp.src('client/index.js')
+    .pipe(browserify({ insertGlobals: true }))
+    .pipe(rename('client.js'))
+    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('public/javascripts'));
+});
+
+gulp.task('browserify-test', ['lint-test'], function() {
+  return gulp.src('test/client/initial_spec.js')
+    .pipe(browserify({ insertGlobals: true }))
+    .pipe(rename('client-test.js'))
+    .pipe(gulp.dest('build'));
+});
+ 
