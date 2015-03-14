ea48e92d7d71c1c8da39388e542ccc132b111382
David G Chaves <davidgchaves@gmail.com>
2015-03-14 12:45:37 +0100

    Add gulp-jshint and setup 'lint-client' and 'lint-test' tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,19 @@
 var gulp   = require('gulp');
 var rename = require('gulp-rename');
 
+// Dev Dependencies
+var jshint = require('gulp-jshint');
+
+// Lint Tasks
+gulp.task('lint-client', function() {
+  return gulp.src('./client/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('lint-test', function() {
+  return gulp.src('./test/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
