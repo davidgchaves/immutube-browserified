f1977e8451152f6cf7854b83d7c00f3ce57fd10e
David G Chaves <davidgchaves@gmail.com>
2015-03-25 10:48:33 +0100

    Add app.js, where the 'real' app lives

---
+++ client/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var $ = require('jquery'),
+    R = require('ramda'),
+    P = require('pointfree-fantasy'),
+    io = require('./io');
+
+io.extendFn();
+
+/*
+ * HELPERS
+ */
+
+var compose = P.compose;
+var map     = P.map;
+var log     = function(x) { console.log(x); return x; };
+var fork    = R.curry(function(error, success, task) { return task.fork(error, success); });
+var setHtml = R.curry(function(sel, x) { return $(sel).html(x); });
+
+/*
+ * PURE
+ */
+
+
+/*
+ * IMPURE
+ */
+
