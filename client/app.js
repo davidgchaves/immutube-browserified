d3ccaa7fe76d99334b76c620f85baa8af518f32f
David G Chaves <davidgchaves@gmail.com>
2015-03-25 11:41:38 +0100

    Add 'pure function' keyUpEventToStream :: DOM -> EventStream DOMEvent

--- client/app.js
+++ client/app.js
@@ -3,6 +3,7 @@
 var $ = require('jquery'),
     R = require('ramda'),
     P = require('pointfree-fantasy'),
+    B = require('baconjs'),
     io = require('./io');
 
 io.extendFn();
@@ -17,12 +18,19 @@ var log     = function(x) { console.log(x); return x; };
 var fork    = R.curry(function(error, success, task) { return task.fork(error, success); });
 var setHtml = R.curry(function(sel, x) { return $(sel).html(x); });
 
+// Helper to curry and switch args order when calling Bacon's fromEvent function
+// We need it because we know the eventName (keyup) and want to curry waiting for the target
+var eventToStream = R.curry(function(eventName, target) { return B.fromEvent(target, eventName); });
+
 /*
  * PURE
  */
 
 //  domSelectorToIO :: Selector -> IO DOM
 var domSelectorToIO = $.toIO();
+
+//  keyUpEventToStream :: DOM -> EventStream DOMEvent
+var keyUpEventToStream = eventToStream('keyup');
 
 /*
  * IMPURE
